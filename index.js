e071cbc7fbac190be77e34b554f2704820c8a6b8
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2020-12-10 18:21:38 +0100

    Added fonts,buttons and linked JS
    Added the "Rubik" google font to the app.
    Included the main.js link to our javascript.
    Added the search box to page

---
+++ index.js
@@ -0,0 +1,45 @@
+//Access keys ==>
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  removeOldImages();
+  const searchTerm = input.value;
+  axios
+    .get(
+      "https://api.unsplash.com/search/photos/?query=${searchTerm}&orientation=squarish&client_id=qYv3cjnECpaACl6AVCaZVaDZMODyA42Kbp7TsYQ4Vbk"
+    )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+const removeOldImages = () => {
+    const images = document.querySelectorAll('a');
+    images.forEach((image)=>{
+        image.remove()
+    });
+};
+
+const generateTags = (data) => {
+  const images = data;
+  images.forEach((image) => {
+    //create link tag
+    const aTag = document.createElement("a");
+    aTag.href = image.links.html;
+    aTag.target = "_blank";
+
+    //create image tag
+    const imgTag = documentcreateElement("img");
+    imgTag.src = image.urls.regular;
+    imgTag.alt = image.alt_description;
+
+    //appending
+    aTag.appendChild(imgTag);
+    document.body.appendChild(aTag);
+  });
+};

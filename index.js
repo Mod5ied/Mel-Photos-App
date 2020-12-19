6a098906b6029831296f485f1137c1384d10169e
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2020-12-19 03:51:10 +0100

    Completed Design layout for desktop and Mobile view
    Fixed background colors issue.
    Assigned colors and custom fonts to both view of mobile and desktop.

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-//Access keys ==>
-const input = document.querySelector("input");
-const form = document.querySelector("form");
+//We first assign our access keys and input element to constants
+const input = document.getElementById("search");
+const accessKey = `hV32rTvyumuVXBlHIJ4SchuzraqM1pjx8oWjab8bIF8`;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  //removeOldImages();
-  const searchTerm = input.value;
-  axios.get(
-      'https://api.unsplash.com/search/photos/?query=${searchTerm}&orientation=squarish&client_id=qYv3cjnECpaACl6AVCaZVaDZMODyA42Kbp7TsYQ4Vbk'
-    )
-    .then(res => res.json)
-    .then(data => console.log(data))
+//Add an event listener to the input element
+input.addEventListener("keyup", (e) => {
+  //we source the user input into a constant and console it
+  const searchString = e.target.value;
+  console.log(searchString);
+
+  //Next we make a get request to the API endpoint
+  const url = `https://api.unsplash.com/search/photos?page=1&query=${searchString}&orientation=squarish&client_id=${accessKey}`;
+  axios
+    .get(url)
+    .then((res) => {
+      generateTags(res.data.results);
+    })
+    .catch((err) => console.log(err));
 });
-/*
-//function to remove tags upon new search
-const removeOldImages = () => {
-  const images = document.querySelectorAll("a");
-  images.forEach((image) => {
-    image.remove();
-  });
-};
-
 
 const generateTags = (data) => {
   const images = data;
@@ -31,7 +27,7 @@ const generateTags = (data) => {
     aTag.target = "_blank";
 
     //create image tag
-    const imgTag = documentcreateElement("img");
+    const imgTag = document.createElement("img");
     imgTag.src = image.urls.regular;
     imgTag.alt = image.alt_description;
 
@@ -40,4 +36,11 @@ const generateTags = (data) => {
     document.body.appendChild(aTag);
   });
 };
-*/
+/*
+//function to remove tags upon new search
+const removeOldImages = () => {
+  const images = document.querySelectorAll("a");
+  images.forEach((image) => {
+    image.remove();
+  });
+};*/
--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-const userInp = document.querySelector('input');
-const form = document.getElementById('myForm');
+//define constants for our accessKeys and url
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const searchKey = userInp.value;
-    axios.get('https://api.unsplash.com/search/photos?page=10&query=${searchKey}&client_id=qYv3cjnECpaACl6AVCaZVaDZMODyA42Kbp7TsYQ4Vbk')
-        .then(function(response){
-            console.log(response);
+const Vueapp = new Vue({
+  el: "#vueapp",
+  data: {},
+  methods: {
+    fetchImages: function () {
+      let searchString = this.$refs.input.value;
+      console.log(searchString);
+      
+      const accessKeys = `hV32rTvyumuVXBlHIJ4SchuzraqM1pjx8oWjab8bIF8`;
+      const url = `https://api.unsplash.com/search/photos?page=1&query=${searchString}&orientation=squarish&client_id=${accessKey}`;
+      axios
+        .get(url)
+        .then((res) => {
+          generateTags(res.data.results);
         })
-})
+        .catch((err) => console.log(err));
+    },
+    generateTags: function (data) {
+      const images = data;
+      images.forEach((image) => {
+        //create link tags to open images in new tab upon click
+        const aTag = document.createElement("a");
+        aTag.href = image.links.html;
+        aTag.target = "_blank";
+        //create the image tag for each image
+        const imgTag = document.createElement("img");
+        imgTag.src = image.urls.regular;
+        imgTag.alt = image.alt_description;
+        //append image tag unto the link tags
+        aTag.appendChild(imgTag);
+        document.body.appendChild(aTag);
+      });
+    },
+  },
+});
